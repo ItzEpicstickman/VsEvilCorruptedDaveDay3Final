a4a8b139d4dfe5d8b86809103bb6964175773498
CyndaquilDAC <65635932+CyndaquilDAC@users.noreply.github.com>
2021-12-06 20:04:25 -0600

    oops i forgor :skull:

--- source/ExtraSongState.hx
+++ source/ExtraSongState.hx
@@ -59,7 +59,7 @@ class ExtraSongState extends MusicBeatState
 		bg.color = 0xFF4965FF;
 		add(bg);
         
-		addWeek(['Sugar-Rush', 'Cycles'], 2, ['bandu-candy', 'bandu-scaredy']);
+		addWeek(['Sugar-Rush', 'Origin', 'Cycles'], 2, ['bandu-candy', 'bandu-origin', 'bandu-scaredy']);
         addWeek(['Thunderstorm', 'Dave-x-Bambi-Shipping-Cute', 'RECOVERED-PROJECT'], 1, ['dave-png', 'dave-good', 'RECOVERED_PROJECT']);
 
         grpSongs = new FlxTypedGroup<Alphabet>();
