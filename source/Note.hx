50ced8ebd8f8e4e184cee934974c8ab0949b9826
CyndaquilDAC <65635932+CyndaquilDAC@users.noreply.github.com>
2021-12-10 17:34:33 -0600

    stuff

--- source/Note.hx
+++ source/Note.hx
@@ -131,7 +131,7 @@ class Note extends FlxSprite
 		
 		switch (PlayState.SONG.song.toLowerCase())
 		{
-			case 'cheating':
+			case 'cheating' | 'disruption':
 				switch (noteData)
 				{
 					case 0:
@@ -177,7 +177,7 @@ class Note extends FlxSprite
 		}
 		switch (PlayState.SONG.song.toLowerCase())
 		{
-			case 'cheating' | 'unfairness' | 'applecore':
+			case 'cheating' | 'unfairness' | 'applecore' | 'disruption':
 				if (Type.getClassName(Type.getClass(FlxG.state)).contains("PlayState"))
 				{
 					var state:PlayState = cast(FlxG.state,PlayState);
