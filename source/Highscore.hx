cf0ca6c4109d71ff1e63478f63b321403d5631e1
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-21 13:35:51 +0200

    Update Highscore.hx

--- source/Highscore.hx
+++ source/Highscore.hx
@@ -18,11 +18,6 @@ class Highscore
 		var daSong:String = formatSong(song, diff);
 		trace("saveScore" + daSong);
 
-		#if !switch
-		NGio.postScore(score, song);
-		#end
-
-
 		if (songScores.exists(daSong))
 			{
 				if (songScores.get(daSong) < score)
@@ -38,12 +33,6 @@ class Highscore
 
 	public static function saveWeekScore(week:Int = 1, score:Int = 0, ?diff:Int = 0, ?char:String = "bf"):Void
 		{
-	
-			#if !switch
-			NGio.postScore(score, "Week " + week);
-			#end
-	
-	
 			var daWeek:String = formatSong('week' + week, diff);
 	
 			if (songScores.exists(daWeek))
