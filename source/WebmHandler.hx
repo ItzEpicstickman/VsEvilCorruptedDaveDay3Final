50ac3a3093132a0b1753c7680507904da7c0f3b4
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-21 13:38:59 +0200

    Update WebmHandler.hx

--- source/WebmHandler.hx
+++ source/WebmHandler.hx
@@ -2,13 +2,13 @@ package;
 
 import flixel.FlxG;
 import openfl.display.Sprite;
-#if desktop
+#if cpp
 import webm.*;
 #end
 
 class WebmHandler
 {
-	#if desktop
+	#if cpp
 	public var webm:WebmPlayer;
 	public var vidPath:String = "";
 	public var io:WebmIo;
@@ -28,7 +28,7 @@ class WebmHandler
 	
 	public function makePlayer():Void
 	{
-		io = new WebmIoFile(vidPath);
+		io = new WebmIoFile(SUtil.getPath() + vidPath);
 		webm = new WebmPlayer();
 		webm.fuck(io, false);
 		webm.addEventListener(WebmEvent.PLAY, function(e) {
@@ -49,7 +49,7 @@ class WebmHandler
 	
 	public function updatePlayer():Void
 	{
-		io = new WebmIoFile(vidPath);
+		io = new WebmIoFile(SUtil.getPath() + vidPath);
 		webm.fuck(io, false);
 	}
 	
@@ -158,12 +158,6 @@ class WebmHandler
 	public function show():Void
 	{
 		webm.visible = true;
-	}
-	#else
-	public var webm:Sprite;
-	public function new()
-	{
-	trace("THIS IS ANDROID! or some shit...");
 	}
 	#end
 }
