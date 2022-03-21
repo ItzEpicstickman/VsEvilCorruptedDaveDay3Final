8a22155807b89fd4b784422366bb4d3edbbeece0
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-21 14:32:01 +0200

    Update EndingState.hx

--- source/EndingState.hx
+++ source/EndingState.hx
@@ -26,14 +26,18 @@ class EndingState extends MusicBeatState
 		end.loadGraphic(Paths.image("dave/" + _ending));
 		FlxG.sound.playMusic(Paths.music(_song),1,true);
 		add(end);
-		FlxG.camera.fade(FlxColor.BLACK, 0.8, true);	
+		FlxG.camera.fade(FlxColor.BLACK, 0.8, true);
+
+                #if android
+	        addVirtualPad(NONE, A);
+                #end	
 	}
 	
 	override public function update(elapsed:Float):Void 
 	{
 		super.update(elapsed);
 		
-		if (FlxG.keys.pressed.ENTER)
+		if (controls.ACCEPT)
 		{
 			endIt();
 		}
